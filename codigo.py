d95be7649d922ecda3fdb09551b6fc74de0890ef
idk <xxnosexx23@gmail.com>
2025-05-20 19:33:12 -0500

    actualizacion al codigo.py: los clientes pueden comprar bebidas despues de haber comprado comida y si tienen el dinero suficiente

--- codigo.py
+++ codigo.py
@@ -3,15 +3,18 @@ import random
 # Variables de cantidad
 tickets = 200
 food = 100
+drinks = 100
 customers = 300
 
 # Costo de los productos
 cost_tickets = 100
 cost_food = 50
+cost_drinks = 25
 
 # Contadores de tickets y comida vendidos
 tickets_sold = 0
 food_sold = 0
+drinks_sold = 0
 
 while customers > 0 and tickets > 0:
     money = random.randint(0, 200)
@@ -28,10 +31,23 @@ while customers > 0 and tickets > 0:
             food_sold += 1
             money -= cost_food
 
+            # Verificar si el cliente puede comprar bebida
+            if drinks > 0 and money >= cost_drinks:
+                drinks -= 1
+                drinks_sold += 1
+                money -= cost_drinks
+
     customers -= 1
 
 # Mostrar los tikets restantes y los vendidos
 print(f"Quedaron {tickets} tickets")
 print(f"Quedaron {food} comidas")
+print(f"Quedaron {drinks} bebidas")
 print(f"Tickets vendidos: {tickets_sold}")
 print(f"Comidas vendidas: {food_sold}")
+print(f"Bebidas vendidas: {drinks_sold}")
+
+# Calcular ganancias
+
+total_sales = (tickets_sold * cost_tickets) + (food_sold * cost_food) + (drinks_sold * cost_drinks)
+print(f"Ganancias totales: {total_sales}")
