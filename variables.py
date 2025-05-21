956ade7e7aae28b69463d1faa8513bcea3a2faaa
idk <xxnosexx23@gmail.com>
2025-05-21 16:30:39 -0500

    se crea el archivo variables.py: se almacenan las variables de los costos, cantidad de productos y el costo total se crea el archivo __pycache__/variables.cpython-313.pyc: se almacena el bytecode de variables.py se modifica el archivo codigo.py: se importa el archivo variables.py, la comida ahora viene en combos (3 combos), los clientes ahora pueden comprar snacks y en la grafica se muestran las ganancias totales

---
+++ variables.py
@@ -0,0 +1,24 @@
+# Variables de cantidad
+tickets = 200
+food_combo_1 = 100
+food_combo_2 = 100
+food_combo_3 = 100
+snacks = 50
+drinks = 100
+customers = 300
+
+# Costo de los productos
+cost_combo_1 = 50
+cost_combo_2 = 75
+cost_combo_3 = 100
+cost_tickets = 150
+cost_snacks = 15
+cost_drinks = 25
+
+# Contadores de tickets y comida vendidos
+snacks_sold = 0
+tickets_sold = 0
+combo_1_sold = 0
+combo_2_sold = 0
+combo_3_sold = 0
+drinks_sold = 0
